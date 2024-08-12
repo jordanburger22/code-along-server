4719f098b1420e16a0649944f5261e546cbe89be
Jordan Burger <jordanburger22@gmail.com>
2024-08-12 17:13:34 +0000

    added ttl index

--- journal-app/models/journal-post.js
+++ journal-app/models/journal-post.js
@@ -15,11 +15,18 @@ const journalPostSchema = new Schema({
         type: Date,
         required: true
     },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
     user: {
         type: String,
         required: true
     }
 })
 
+const JournalPost = mongoose.model('JournalPost', journalPostSchema)
 
-module.exports = mongoose.model('JournalPost', journalPostSchema)
+JournalPost.collection.createIndex({ createdAt: 1}, { expireAfterSeconds: 2592000 })
+
+module.exports = JournalPost
